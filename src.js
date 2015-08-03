3f950b4037225a05ed839121c8b68c9e7e6ad75a
keithhopkins <knhopkins412@gmail.com>
2015-08-03 14:24:33 -0600

    complete

--- src.js
+++ src.js
@@ -1,7 +1,60 @@
 var yourCode = {
-  commafy: function(n) {},
+  commafy: function(n) {
+    var str='';
+    for(var i=1;i<n;i++){
+      str+=i+',';
+    }
+    return str+n;
+  },
 
-  joinToString: function(array) {}
-}
+  joinToString: function(array) {
+    var str='';
+    for(var i=0;i<array.length-1;i++){
+      str+=array[i]+' and ';
+    }
+    str+=array[array.length-1]+' oh my.';
+    return str;
+  },
+
+  printBracketedNumbers: function(num){
+    var str = '';
+    for(var i=0;i<=num;i++){
+      str+='['+i+']';
+    }
+    return str;
+  },
+
+  sumOfN: function(num){
+    var sum=0;
+    for(var i=1;i<=num;i++)
+      sum+=i;
+    return sum;
+  },
+
+  moviesFor: function(name){
+    switch(name){
+      case "Robin Williams": return "Jumanji, Hook, Good Will Hunting";
+      case "Sandra Bullock": return "Gravity, The Lake House, The Blind Side";
+      case "Daniel Radcliffe": return "All of the Harry Potters";
+      default: return "Please provide an actor or actress";
+    }
+  },
+
+  joinWithForAndIndex: function(array,token){
+    var str='';
+    for(var i=0;i<array.length;i++){
+      str+=array[i]+i;
+    }
+    return str;
+  }
+
+  joinWithForAndToken: function(array){
+    var str='';
+    for(var i=0;i<array.length-1;i++){
+      str+=array[i]+token;
+    }
+    return str+array[array.length-1];
+  }
+};
 
 module.exports = yourCode;
